86fce1bddec0481fecfb332b9382fa9684ce6213
Vinicius Rocha <vinirocha05@gmail.com>
2022-06-23 07:40:48 -0300

    criando classe Position

---
+++ src/BoardGame/Position.java
@@ -0,0 +1,34 @@
+package BoardGame;
+
+public class Position {
+	private Integer row;
+	private Integer column;
+	public Position(Integer row, Integer column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	public Integer getRow() {
+		return row;
+	}
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+	public Integer getColumn() {
+		return column;
+	}
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+	
+	public void setValues(Integer row, Integer column) {
+		this.column = column;
+		this.row = row;
+	}
+	
+	@Override
+	public String toString() {
+		return row + "," +column;
+	}
+	
+}
